90145bc875ef49542cb3b032a594e96e4d7dbc88
Gabe <jflores353@gmail.com>
2019-12-28 12:31:47 -0600

    Designed Header layout and styling with Sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
 import React from "react"
 import "../styles/styles.scss"
 
-const IndexPage = () => <p>Home</p>
+//Import Components
+import Header from "../components/header"
+
+const IndexPage = () => (
+  <div>
+    <Header />
+  </div>
+)
 
 export default IndexPage
