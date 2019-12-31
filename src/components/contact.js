b50e646e26e1891052933a15b7dfaa5af77b3375
Gabe <jflores353@gmail.com>
2019-12-31 14:38:10 -0600

    installed gatsby-plugin-netlify-cms for form handling

--- src/components/contact.js
+++ src/components/contact.js
@@ -35,7 +35,7 @@ export default function Contact() {
       <form
         name="contact"
         method="post"
-        action="/thanks/"
+        action="/"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
         onSubmit={handleSubmit}
